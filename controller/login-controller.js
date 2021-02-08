fd1f560acaee65e52ce04bb53c5b0ce286e5b43a
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-08 16:15:50 -0600

    data tested but not going through

--- controller/login-controller.js
+++ controller/login-controller.js
@@ -36,7 +36,7 @@ router.post("/", (req, res, next) => {
           else{res.send(`email was taken ${req}`)}
         })
       }
-      else{res.send(req)}
+      else{res.send(console.log(`sending log`))}
     
     })
     
