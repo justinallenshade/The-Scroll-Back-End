be018c524d16e433f6666f8604a3395ca82562cb
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-08 18:51:58 -0600

    data tested but not going through

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const postController = require("./controller/post-controller");
 const loginController = require('./controller/login-controller')
+
 const cors = require("cors");
-app.use(cors());
+var whitelist = ['https://the-scroll-back-end.herokuapp.com/', 'http://localhost:3000/']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+ 
+app.use(cors(whitelist, corsOptionsDelegate));
 
 // middleware
 app.use(express.json());
