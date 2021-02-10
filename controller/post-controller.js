0c6ca2601817945585a83f077653e504f85b2f55
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-10 01:09:39 -0600

    cru is done

--- controller/post-controller.js
+++ controller/post-controller.js
@@ -11,15 +11,15 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   postRouter
     .create(req.body)
-    .then((router) => res.json(router))
+    // .then((router) => res.json(router))
 });
 
 // update a single post
 router.put("/", (req, res) => {
   postRouter
     .findOneAndUpdate({ _id: req.body.id }, req.body)
-    .then(postRouter.find({})
-  // ).then((router) => res.json(router));
+    // .then(postRouter.find({}))
+    // .then((router) => res.json(router));
 });
 
 // delete a person by id
